6c32a4a93da695d910350185ee244fa0fedf9161
kai.guo <gkmails@126.com>
2023-12-15 18:33:17 +0800

    添加代码

--- Example03/src/main.c
+++ Example03/src/main.c
@@ -54,10 +54,42 @@ GLUSboolean init(GLUSvoid)
 	glBindBuffer(GL_ARRAY_BUFFER, g_vertex_vbo);
 	glBufferData(GL_ARRAY_BUFFER, plane.numberIndices * 4 * sizeof(GLfloat), (GLfloat*)plane.vertices, GL_STATIC_DRAW);
 
+	glGenBuffers(1, &g_text_coord_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, &g_text_coord_vbo);
+	glBufferData(GL_ARRAY_BUFFER, plane.numberVertices* 2*sizeof(GLfloat), (GLfloat*)plane.texCoords, GL_STATIC_DRAW);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glGenBuffers(1, &g_indices_vbo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, &g_indices_vbo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, plane.numberIndices* sizeof(GLint), (GLint*)plane.indices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	glusShapeDestroyf(&plane);
+
+	glUseProgram(g_programe.program);
+	glGenVertexArrays(1, &g_vao);
+	glBindVertexArray(g_vao);
+	glBindBuffer(GL_ARRAY_BUFFER, g_vertex_vbo);
+	glVertexAttribPointer(g_vertex_location, 4, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(g_vertex_location);
+	glBindBuffer(GL_ARRAY_BUFFER, g_text_coord_vbo);
+	glVertexAttribPointer(g_texture_location, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(g_texture_location);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g_indices_vbo);
 
 
+	glBindTexture(GL_TEXTURE_2D, g_texture);
+	glUniform1i(g_texture_location, 0);
 
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	return GLUS_TRUE;
 
+}
 
+int main()
+{
 
 }
